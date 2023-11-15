55433b64837825f5f6d67bedf444f9c724b90ac8
coder_layne <layne.zhuang@gmail.com>
2023-11-15 20:34:46 +0800

    Revert "fix: temporary fix for compatibility issue with  on Node 14 #36"
    This reverts commit bdd30ab0500beb9b1892963365f068124d30f906.

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import type { Buffer } from 'node:buffer'
 import { type Alias, type Plugin, type ResolvedConfig, createFilter, preprocessCSS } from 'vite'
 import { type PluginContext } from 'rollup'
 import { interpolateName } from 'loader-utils'
-
-// @ts-expect-error string.prototype.replaceall has no typescript declaration
-import replaceAll from 'string.prototype.replaceall'
 import { checkFormats, getAssetContent, getCaptured, getFileBase64 } from './utils'
 import { ASSETS_IMPORTER_RE, CSS_LANGS_RE, DEFAULT_ASSETS_RE, cssImageSetRE, cssUrlRE } from './constants'
 import { resolveCompiler } from './compiler'
@@ -156,7 +153,7 @@ export default function VitePluginLibAssets(options: Options = {}): Plugin {
       .forEach((name) => {
         let updated = updatedSourceMap[name]
         Array.from(assetsInStyle).forEach(([base64, asset]) => {
-          updated = replaceAll(updated, base64, publicUrl ? asset : `./${asset}`)
+          updated = updated.replaceAll(base64, publicUrl ? asset : `./${asset}`)
         })
 
         if (updatedSourceMap[name] !== updated)
@@ -182,7 +179,7 @@ export default function VitePluginLibAssets(options: Options = {}): Plugin {
           const relativeAsset = path.posix.relative(fileDir, asset)
           const originalAsset = `./${asset}`
           if (asset !== relativeAsset && updated.includes(originalAsset))
-            updated = replaceAll(updated, originalAsset, relativeAsset)
+            updated = updated.replaceAll(originalAsset, relativeAsset)
         })
 
         if (updatedSourceMap[name] !== updated)
