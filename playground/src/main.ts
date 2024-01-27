d18065faba97a1341b14243e0930fdfc7b889183
layne.zhuang <zxl198907@163.com>
2024-01-27 11:30:58 +0800

    chore: fix ci lint error in the playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -3,6 +3,7 @@ import app from './app.vue'
 // import logo from './assets/vite-logo.svg'
 import './styles/index.css'
 
+// eslint-disable-next-line no-console
 import('./async').then(console.log.bind(null, 'async'))
 
 export {
